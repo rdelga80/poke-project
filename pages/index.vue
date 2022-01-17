a8b8164abfd23902584d48061b2ecb8edb38f59a
edlopez000 <lopez.edward@outlook.com>
2022-01-17 03:49:06 -0500

    Implemented a SCSS architecture and an attempt at a type scale

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,11 @@
 <template>
   <div>
-    <p>Home page</p>
+    <h1>Hello Heading 1</h1>
+    <h2>Hello Heading 2</h2>
+    <h3>Hello Heading 3</h3>
+    <h4>Hello Heading 4</h4>
+    <h5>Hello Heading 5</h5>
+    <p>Paragraph Text</p>
   </div>
 </template>
 
